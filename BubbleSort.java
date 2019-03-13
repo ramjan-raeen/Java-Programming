d477baffb461571cd4247a1ceaf1fdedfde141c7
Ramjan Raeen <40716654+ramjan-raeen@users.noreply.github.com>
2019-03-13 18:38:06 +0530

    Bubble sorting code
    Bubble Sort Implementation in Java
    here is the Java program to implement bubble sort algorithm using Java programming language. Don't surprise with import of java.util.Array, we have not used it's sort method here, instead, it is used to print arrays in a readable format. I have created a swap function to swap numbers and improve readability of code, if you don't like you can in-line the code in the swap method inside if statement of inner loop. Though I have used main method for testing, as it demonstrate better, I would suggest you to write some unit test case for your bubble sort implementation

---
+++ BubbleSort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Arrays;
+//This is BubbleSort programe.
+// Time complexcity
+// worst case:---> O(n^2)
+// Best case:---> O(n)
+// Avg case:---->O(n^2)
+class BubbleSort{
+	public static void main(String []args){
+		System.out.println("Enter a number to elements size: ");
+		Scanner scn = new Scanner(System.in);
+		int size = new Scanner(System.in).nextInt();
+		int []arr = new int[size];
+		for(int i=0; i < arr.length; i++){
+			System.out.printf("Enter  %d Element: ", i+1);
+			arr[i] = scn.nextInt();
+		}
+		System.out.println("Enter Unsorted Elements: "+ Arrays.toString(arr));
+		/*for(int i=0; i<arr.length; i++){
+			System.out.printf(" "+arr[i]);
+		}*/
+		BubbleSortArray(arr);
+		System.out.println("Sorted Elements: "+ Arrays.toString(arr));
+	}
+	public static void BubbleSortArray(int []arr){
+		for(int i=0; i< arr.length; i++){
+			for(int j= arr.length-1; j>i; j--){
+				if(arr[j] < arr[j-1]){
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+			}
+		}
+		//System.out.println("Sorted Elements: "+ Arrays.toString(arr));
+	}
+}
